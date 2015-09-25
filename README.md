e29ab4588caefcf0694b77da29b2303484b68380
Daniel Sellers <daniel@designfrontier.net>
2015-09-25 00:12:09 -0600

    added styles and links to the slides

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 The train as an array example
 
+[Slides are here](http://slides.com/danielsellers/deck-3c5aae95-d7e1-4a22-aa8e-2e8d32c4fd8b)
+
 ------
 
 Built with [monumentjs](http://monument.ansble.com)
--- templates/main.js
+++ templates/main.js
@@ -1,6 +1,6 @@
 (function(){function main(it
 /**/) {
-var out='<html><head> <link rel="stylesheet" href="/dist/main.css" /></head> <body> </body> <script src="/components/stubs/index.js"></script></html>';return out;
+var out='<html><head> <link rel="stylesheet" href="/dist/main.css" /></head> <body> <h1>Utah JS Arrays</h1> <a href="http://slides.com/danielsellers/deck-3c5aae95-d7e1-4a22-aa8e-2e8d32c4fd8b"> The Slides</a> </body> <script src="/components/stubs/index.js"></script></html>';return out;
 }var itself=main, _encodeHTML=(function (doNotSkipEncoded) {
 		var encodeHTMLRules = { "&": "&#38;", "<": "&#60;", ">": "&#62;", '"': "&#34;", "'": "&#39;", "/": "&#47;" },
 			matchHTML = doNotSkipEncoded ? /[&<>"'\/]/g : /&(?!#?\w+;)|<|>|"|'|\//g;
